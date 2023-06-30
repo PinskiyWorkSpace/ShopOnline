11e8658d4a9eede757a6e7e3751531feeb5610fb
unknown <Pinsky-FS@yandex.ru>
2023-06-30 19:01:51 +0300

    fix M7.L4

---
+++ docs/css/js/render.js
@@ -0,0 +1,1 @@
+import{banerTitle,timerTitle,createWrapper,createDay,createHours,createMinutes,createSeconds}from"./create.js";export const render=e=>{const r=banerTitle(),t=createWrapper(),a=timerTitle(),c=createDay(),n=createHours(),p=createMinutes(),i=createSeconds();return t.append(c,n,p,i),e.append(r,a,t),e};
