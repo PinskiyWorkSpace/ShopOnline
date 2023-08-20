87b844dda3308847a74d8e17bea9e9b82395ef65
Filipp Pinskiy <pinskiy.fs@ya.ru>
2023-08-20 13:16:32 +0300

    Выполнено задание

--- dist/js/create.js
+++ dist/js/create.js
@@ -1,104 +1,81 @@
-export const banerTitle = () => {
-  const h1 = document.createElement('h1');
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.timerTitle = exports.createWrapper = exports.createSeconds = exports.createMinutes = exports.createHours = exports.createDay = exports.banerTitle = void 0;
+var banerTitle = function banerTitle() {
+  var h1 = document.createElement('h1');
   h1.classList.add('banner__title');
   h1.textContent = '50% на все ноутбуки';
   return h1;
 };
-
-const createParagraf = () => {
-  const p = document.createElement('p');
+exports.banerTitle = banerTitle;
+var createParagraf = function createParagraf() {
+  var p = document.createElement('p');
   p.classList.add('timer__item');
   return p;
 };
-
-const createSpan = () => {
-  const span = document.createElement('span');
+var createSpan = function createSpan() {
+  var span = document.createElement('span');
   return span;
 };
-
-export const createWrapper = () => {
-  const div = document.createElement('div');
+var createWrapper = function createWrapper() {
+  var div = document.createElement('div');
   div.classList.add('timer__block');
   return div;
 };
-
-export const timerTitle = () => {
-  const title = createParagraf();
+exports.createWrapper = createWrapper;
+var timerTitle = function timerTitle() {
+  var title = createParagraf();
   title.classList.remove('timer__item');
   title.classList.add('timer__title');
   title.textContent = 'До конца акции:';
   return title;
 };
-
-export const createDay = () => {
-  const p = createParagraf();
+exports.timerTitle = timerTitle;
+var createDay = function createDay() {
+  var p = createParagraf();
   p.classList.add('timer__item_days');
-
-  const spanCount = createSpan();
+  var spanCount = createSpan();
   spanCount.classList.add('timer__count', 'timer__count_days');
-
-  const spanUnits = createSpan();
+  var spanUnits = createSpan();
   spanUnits.classList.add('timer__units', 'timer__units_days');
-
   p.append(spanCount, spanUnits);
-
   return p;
 };
-
-export const createHours = () => {
-  const p = createParagraf();
+exports.createDay = createDay;
+var createHours = function createHours() {
+  var p = createParagraf();
   p.classList.add('timer__item_hours');
-
-  const spanCount = createSpan();
+  var spanCount = createSpan();
   spanCount.classList.add('timer__count', 'timer__count_hours');
-
-  const spanUnits = createSpan();
+  var spanUnits = createSpan();
   spanUnits.classList.add('timer__units', 'timer__units_hours');
-
   p.append(spanCount, spanUnits);
-
   return p;
 };
-
-export const createMinutes = () => {
-  const p = createParagraf();
+exports.createHours = createHours;
+var createMinutes = function createMinutes() {
+  var p = createParagraf();
   p.classList.add('timer__item_minutes');
-
-  const spanCount = createSpan();
+  var spanCount = createSpan();
   spanCount.classList.add('timer__count', 'timer__count_minutes');
-
-  const spanUnits = createSpan();
+  var spanUnits = createSpan();
   spanUnits.classList.add('timer__units', 'timer__units_minutes');
-
   p.append(spanCount, spanUnits);
-
   return p;
 };
-
-export const createSeconds = () => {
-  const p = createParagraf();
+exports.createMinutes = createMinutes;
+var createSeconds = function createSeconds() {
+  var p = createParagraf();
   p.classList.add('timer__item_seconds');
-
-  const spanCount = createSpan();
+  var spanCount = createSpan();
   spanCount.classList.add('timer__count', 'timer__count_seconds');
-
-  const spanUnits = createSpan();
+  var spanUnits = createSpan();
   spanUnits.classList.add('timer__units', 'timer__units_seconds');
-
   p.append(spanCount, spanUnits);
-
   return p;
 };
-
-
-
-
-
-
-
-
-
-
-
-
+exports.createSeconds = createSeconds;
 //# sourceMappingURL=../maps/create.js.map
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,37 +1,24 @@
-import {render} from "./render.js";
-import {
-  getTimeRemaining,
-  formatTime,
-  declination
-} from "./control.js";
+"use strict";
 
-
-
-const timer = () => {
-  const timerMain = document.querySelector('.timer');
-  render(timerMain);
-
-  const timerTitle = document.querySelector('.timer__title');
-  const timerItem = document.querySelectorAll('.timer__item ');
-
-  const itemDays = document.querySelector('.timer__item_days');
-  const timeDays = document.querySelector('.timer__count_days');
-  const textDays = document.querySelector('.timer__units_days');
-
-  const timeHours = document.querySelector('.timer__count_hours');
-  const textHours = document.querySelector('.timer__units_hours');
-
-  const timeMinutes = document.querySelector('.timer__count_minutes');
-  const textMinutes = document.querySelector('.timer__units_minutes');
-
-  const itemseconds = document.querySelector('.timer__item_seconds');
-  const timeSeconds = document.querySelector('.timer__count_seconds');
-  const textSeconds = document.querySelector('.timer__units_seconds');
-
-
-  const start = () => {
-    const timer = getTimeRemaining();
-
+var _render = require("./render.js");
+var _control = require("./control.js");
+var timer = function timer() {
+  var timerMain = document.querySelector('.timer');
+  (0, _render.render)(timerMain);
+  var timerTitle = document.querySelector('.timer__title');
+  var timerItem = document.querySelectorAll('.timer__item ');
+  var itemDays = document.querySelector('.timer__item_days');
+  var timeDays = document.querySelector('.timer__count_days');
+  var textDays = document.querySelector('.timer__units_days');
+  var timeHours = document.querySelector('.timer__count_hours');
+  var textHours = document.querySelector('.timer__units_hours');
+  var timeMinutes = document.querySelector('.timer__count_minutes');
+  var textMinutes = document.querySelector('.timer__units_minutes');
+  var itemseconds = document.querySelector('.timer__item_seconds');
+  var timeSeconds = document.querySelector('.timer__count_seconds');
+  var textSeconds = document.querySelector('.timer__units_seconds');
+  var start = function start() {
+    var timer = (0, _control.getTimeRemaining)();
     if (timer.day === 0) {
       itemDays.classList.add('hiden');
       itemseconds.classList.remove('hiden');
@@ -39,32 +26,24 @@ const timer = () => {
       itemDays.classList.remove('hiden');
       itemseconds.classList.add('hiden');
     }
-
-    textDays.textContent = declination(timer.day, ['день', 'дня', 'дней']);
-    textHours.textContent = declination(timer.hour, ['час', 'часа', 'часов']);
-    textMinutes.textContent = declination(timer.minutes, ['минута', 'минуты', 'минут']);
-    textSeconds.textContent = declination(timer.seconds, ['секунда', 'секунды', 'секунд']);
-
-    timeDays.textContent = formatTime(timer.day);
-    timeHours.textContent = formatTime(timer.hour);
-    timeMinutes.textContent = formatTime(timer.minutes);
-    timeSeconds.textContent = formatTime(timer.seconds);
-
-    const intervalId = setTimeout(start, 1000);
-
+    textDays.textContent = (0, _control.declination)(timer.day, ['день', 'дня', 'дней']);
+    textHours.textContent = (0, _control.declination)(timer.hour, ['час', 'часа', 'часов']);
+    textMinutes.textContent = (0, _control.declination)(timer.minutes, ['минута', 'минуты', 'минут']);
+    textSeconds.textContent = (0, _control.declination)(timer.seconds, ['секунда', 'секунды', 'секунд']);
+    timeDays.textContent = (0, _control.formatTime)(timer.day);
+    timeHours.textContent = (0, _control.formatTime)(timer.hour);
+    timeMinutes.textContent = (0, _control.formatTime)(timer.minutes);
+    timeSeconds.textContent = (0, _control.formatTime)(timer.seconds);
+    var intervalId = setTimeout(start, 1000);
     if (timer.timeRemaining <= 0) {
       clearTimeout(intervalId);
       timerTitle.remove();
-      timerItem.forEach(el => {
+      timerItem.forEach(function (el) {
         el.remove();
       });
     }
-
   };
-
   start();
 };
-
 timer();
-
 //# sourceMappingURL=../maps/main.js.map
